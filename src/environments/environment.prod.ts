11bf3592a753533ff5c1aea0b1611af378493525
neguewatt <neguewatt@me.com>
2021-05-18 18:23:55 +0200

    ajout firebase a l'environnement de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBzpo_i7EaNUOOh30g_Icp8OBwTTpWOPLI',
+    authDomain: 'chefsbook-test.firebaseapp.com',
+    databaseURL: 'https://chefsbook-test.firebaseio.com',
+    projectId: 'chefsbook-test',
+    storageBucket: 'chefsbook-test.appspot.com',
+    messagingSenderId: '425415146991',
+    appId: '1:425415146991:web:2b4ab044225c1ef0160a1a',
+    measurementId: 'G-TQDV32NCX5'
+  }
 };
